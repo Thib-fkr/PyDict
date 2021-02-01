1799c42abff31bcc4e55151ba6b38cdf467b49f3
Thib-fkr <71818209+Thib-fkr@users.noreply.github.com>
2021-02-01 11:37:22 +0100

    test template

--- connectionTest.py
+++ connectionTest.py
@@ -3,3 +3,12 @@ from Classes.base import Session, engine, Base
 from Classes.idClass import Word_ID
 from Classes.languageClass import French, English, Dutch
 from Classes.synonymsClass import FrenchSy, EnglishSy, DutchSy
+
+Base.metadata.create_all(engine)
+
+session = Session()
+
+# Test goes here
+
+session.commit()
+session.close()

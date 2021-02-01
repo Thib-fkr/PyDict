bc9f46cd4324192fa922c12338bac3d383776bd8
Thib-fkr <71818209+Thib-fkr@users.noreply.github.com>
2021-02-01 10:36:07 +0100

    fixed imports

--- Classes/SynonymsClass.py
+++ Classes/SynonymsClass.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from base import Base
+from Classes.base import Base
 
 class FrenchSy(Base):
     """"""
---
+++ connectionTest.py
@@ -0,0 +1,5 @@
+from Classes.base import Session, engine, Base
+
+from Classes.idClass import Word_ID
+from Classes.languageClass import French, English, Dutch
+from Classes.synonymsClass import FrenchSy, EnglishSy, DutchSy

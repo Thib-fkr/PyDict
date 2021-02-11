a002bf3f3f4d0a17871665bcf9c60d563b20b6fa
Thib-fkr <71818209+Thib-fkr@users.noreply.github.com>
2021-02-11 11:19:10 +0100

    testing connexion

--- connectionTest.py
+++ connectionTest.py
@@ -8,13 +8,27 @@ Base.metadata.create_all(engine)
 
 session = Session()
 
-# Test goes here
+# Test goes here ------------------------------------------------------
 
 # Reference word
+hello = Word_ID('hello')
+bye = Word_ID('bye')
 
 # Word in a specific language
+Ehello = English('hello','standart greeting', '', hello)
+Fbonjour = French('bonjour', 'Salutation matinale ou dans le courant de l\'après midi', '', '', hello)
+Dtotziens = Dutch('tot ziens', '', '', '', bye)
 
 # Synonyms
+FSbonjour = FrenchSy('salut',Fbonjour)
+
+# add to session
+session.add(hello)
+session.add(bye)
+session.add(Ehello)
+session.add(Fbonjour)
+session.add(Dtotziens)
+session.add(FSbonjour)
 
 session.commit()
 session.close()

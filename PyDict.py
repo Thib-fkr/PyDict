a6f2edc3574540ffc63eea4677413e5299c555bb
Thib-fkr <71818209+Thib-fkr@users.noreply.github.com>
2021-02-23 11:17:01 +0100

    introducted argparse

--- PyDict.py
+++ PyDict.py
@@ -1,23 +1,16 @@
 # Main file of the PYDICT project
 #
 #
-from Functions.functionModule import dynamicQuery, getTable, getColumns, existingEntryQuery, addRow, updateRow
-from Classes.baseTest import Session, engine, Base
-from Classes.languageClass import *
-from Classes.idClass import *
+import sys
+import argparse
 
-session = Session()
 
-query = {'definition' : 'from meedelen',
-        'pos' : 'common noun'}
-value = {'gender' : 'de'}
-#session.query(Word_ID).filter_by(**query).one()
 """
+session = Session()
 print(session.query(Word_ID) \
     .join(French) \
     .filter_by(**query) \
     .all()[0].french_word.word)
 """
-#addRow(session, 'dutch', 'mededeling', 'communication', query, addWID=True)
-updateRow(session, 'dutch',query, value)
-print(dynamicQuery(session, 'dutch', query))
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
